70aac9787bf69254c9a28ee9196b917535cc10aa
rrandol3@fau.edu <rrandol3@fau.edu>
2020-08-09 21:30:28 -0400

    Add project files.

---
+++ 01 - Arrays/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01___Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1,2,3,4,5,6 };
+            Console.WriteLine(BinarySearch(arr, 3));
+        }
+
+        //Implement Binary Search on a Sorted Array
+        public static int BinarySearch(int[] arr, int target)
+        {
+            int low = 0;
+            int high = arr.Length - 1;
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+                if (arr[mid] == target)
+                {
+                    return mid;
+                }
+                else if (arr[mid] > target)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
